d5ea62056d3ffd68f93ff3776c722f10e482ab03
chenyunchen <yunchen@nisra.net>
2015-07-31 12:30:02 +0800

    Default content init with empty store,reset default content when store content first time emitchange!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var React = require('react');
 var isMobile = require('./utils/isMobile')();
 var CodeMirror;
 var jsLintAddon;
+var initCount = 0;
 
 if (!isMobile) {
   CodeMirror = require('codemirror');
@@ -61,11 +62,15 @@ var CodeMirrorEditor = React.createClass({
   },
 
   componentWillReceiveProps: function(nextProps) {
-    var value = nextProps.value
-    if (this._editor) {
-      if (value != null) {
-        if (this._editor.getValue() !== value) {
-          this._editor.setValue(value);
+    initCount++;
+    //default content init with empty store,reset default content when store content first time emitchange!
+    if(initCount === 2){
+      var value = nextProps.defaultValue
+      if (this._editor) {
+        if (value != null) {
+          if (this._editor.getValue() !== value) {
+            this._editor.setValue(value);
+          }
         }
       }
     }
